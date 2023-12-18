0faa8e8d8db188fa8d9246c70e29f9e52b6932ec
rishabh-9695 <rishabhrt9596@gmail.com>
2023-12-18 17:42:20 +0530

    commit 2

--- app/app/test.py
+++ app/app/test.py
@@ -5,5 +5,6 @@ from app import calc
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
-        res = calc.add(5,5)
-        self.assertEqual(res,10)
+        res = calc.add(5, 5)
+        self.assertEqual(res, 10)
+        
